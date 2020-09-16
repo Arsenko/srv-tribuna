10a3bc17e54791e1a3abd85eab3c522d6f07d5d3
Arsenko <minsenka@gmail.com>
2020-09-16 21:56:49 +0300

    infinity loop fix

--- src/repos/IdeaRepositoryBasic.kt
+++ src/repos/IdeaRepositoryBasic.kt
@@ -123,11 +123,9 @@ class IdeaRepositoryBasic : IdeaRepository {
 
     override suspend fun getIdeaWithAuthor(authorName: String): List<Idea> {
         var temp: MutableList<Idea> = mutableListOf()
-        for (i in idealist.indices) {
-            if (idealist[i].authorName==authorName) {
+        for (i in 0 until idealist.size) {
+            if (authorName==idealist[i].authorName) {
                 temp.add(idealist[i])
-            }else{
-                temp.add(idealist[2])
             }
         }
         return temp.toList()
