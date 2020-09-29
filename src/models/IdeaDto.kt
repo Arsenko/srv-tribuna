e895259279ed427ca0dac7859392bcac9e1c683f
Arsenko <minsenka@gmail.com>
2020-09-29 22:42:36 +0300

    release change route/ minor changes

--- src/models/IdeaDto.kt
+++ src/models/IdeaDto.kt
@@ -4,9 +4,7 @@ import java.util.*
 
 class IdeaDto(
         val id: Int,
-        val authorName: String,
-        val authorStatus: Boolean?, //true=prom false=hater null=neutral
-        val authorDrawable: ByteArray,
+        val author:AuthorDto,
         val ideaText: String,
         val ideaDate: Date,
         val link: String?,
@@ -19,24 +17,18 @@ class IdeaDto(
             val temp=idea.ideaReaction.find{
                 it.authorName==userName
             }
-            val status=when(author.authorStatus){
-                in 26..2147483647 -> true
-                in -2147483648..-26 -> false
-                in 25..25 -> null
-                else -> null
-            }
             return if(idea.id!=null) {
                 if (temp != null) {
                     if (temp.promoted) {
-                        IdeaDto(idea.id, idea.authorName,status,author.authorDrawable ,idea.ideaText, idea.ideaDate, idea.link, idea.ideaDrawable, true, false)
+                        IdeaDto(idea.id, AuthorDto.generateDto(author),idea.ideaText, idea.ideaDate, idea.link, idea.ideaDrawable, true, false)
                     }else{
-                        IdeaDto(idea.id, idea.authorName,status,author.authorDrawable , idea.ideaText, idea.ideaDate, idea.link, idea.ideaDrawable, false, true)
+                        IdeaDto(idea.id, AuthorDto.generateDto(author) , idea.ideaText, idea.ideaDate, idea.link, idea.ideaDrawable, false, true)
                     }
                 }else{
-                    IdeaDto(idea.id, idea.authorName,status,author.authorDrawable , idea.ideaText, idea.ideaDate, idea.link, idea.ideaDrawable, false, false)
+                    IdeaDto(idea.id, AuthorDto.generateDto(author) , idea.ideaText, idea.ideaDate, idea.link, idea.ideaDrawable, false, false)
                 }
             }else {
-                IdeaDto(-1,idea.authorName,status,author.authorDrawable , idea.ideaText, idea.ideaDate, idea.link, idea.ideaDrawable, false, false)
+                IdeaDto(-1,AuthorDto.generateDto(author) , idea.ideaText, idea.ideaDate, idea.link, idea.ideaDrawable, false, false)
             }
         }
     }
